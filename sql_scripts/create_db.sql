31e0249628ec25a05e402026f200f36adf50ba84
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2024-10-05 12:14:33 -0400

    Update create_db script

--- sql_scripts/create_db.sql
+++ sql_scripts/create_db.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE IF NOT EXISTS studentsync;
 USE studentsync;
+DROP TABLE IF EXISTS user;
 CREATE TABLE IF NOT EXISTS user(
 	username VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL,
