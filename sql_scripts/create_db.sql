bde97a556586ad975831ded6fad699522b308abc
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2024-10-11 14:33:58 -0400

    Create database table for storing teachers

--- sql_scripts/create_db.sql
+++ sql_scripts/create_db.sql
@@ -2,6 +2,7 @@ CREATE DATABASE IF NOT EXISTS studentsync;
 USE studentsync;
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS student;
+DROP TABLE IF EXISTS teacher;
 CREATE TABLE IF NOT EXISTS user(
 	username VARCHAR(50) NOT NULL,
     password VARCHAR(100) NOT NULL,
@@ -12,5 +13,12 @@ CREATE TABLE IF NOT EXISTS student(
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     birth_date DATE,
+    PRIMARY KEY (id)
+);
+CREATE TABLE IF NOT EXISTS teacher(
+	id VARCHAR(50) NOT NULL,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    start_date DATE,
     PRIMARY KEY (id)
 );
